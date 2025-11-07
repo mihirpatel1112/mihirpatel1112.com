ecd6ab5eb88b2cabdb412480d0d69a59eaa44a01
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2025-11-07 21:09:56 +1100

    list item and hero

---
+++ components/hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  return (
+    <div className="container">
+      <div className="py-24">
+        <p className="text-muted-foreground text-lg font-medium">
+          Hello, I&apos;m
+        </p>
+        <h1 className="scroll-m-20 text-6xl sm:text-7xl lg:text-8xl font-extrabold tracking-tight">
+          Mihir Patel
+        </h1>
+
+        <div className="space-y-4 text-lg text-muted-foreground leading-relaxed border-l-4 border-border pl-2 md:pl-4 sm:pl-6 my-4">
+          <p>
+            I craft elegant solutions to complex problems, specializing in
+            building beautiful and functional digital experiences.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/nav.tsx
+++ components/nav.tsx
@@ -16,16 +16,18 @@ export default function Nav({
   items: Array<{ title: string; link: string }>;
 }) {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        {items.map((it, idx) => (
-          <NavigationMenuItem key={idx}>
-            <NavigationMenuLink asChild>
-              <Link href={it.link}>{it.title}</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="flex justify-end">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {items.map((it, idx) => (
+            <NavigationMenuItem key={idx}>
+              <NavigationMenuLink asChild>
+                <Link href={it.link}>{it.title}</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 }
