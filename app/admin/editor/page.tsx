0aa833fb38e8fcee50d726383e96efdc7fe89be5
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2025-11-16 14:24:16 +1100

    more stuff added

---
+++ app/admin/editor/page.tsx
@@ -0,0 +1,9 @@
+import Paper from "@/components/paper";
+
+export default function page() {
+  return (
+    <Paper>
+      <div className="bg-slate-100 rounded-lg p-2"></div>
+    </Paper>
+  );
+}
