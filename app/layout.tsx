e6cecb02cfcd1422800f9f670afa12b040a55067
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2026-03-15 14:21:10 +1100

    content update instant

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,8 @@ export const metadata: Metadata = {
   manifest: "/site.webmanifest",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
