0b1b5c1af019d73f6f28db82a161a7fb42f76381
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2026-03-25 22:13:26 +1100

    fix blog

--- constants/nav.js
+++ constants/nav.js
@@ -1,5 +1,4 @@
 export const navItems = [
-  { title: "Blog", link: "/blog" },
   { title: "Articles", link: "/articles" },
   { title: "Projects", link: "/projects" },
   { title: "Gallery", link: "/gallery" },
